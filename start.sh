00ccb11282de047f52cc562966e9ba1852526811
Mahmoud <mahmoud.hachem@sword-group.com>
2018-10-27 12:24:53 +0300

    Unit services

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting the Eureka Server"
+echo "********************************************************"
+sudo /usr/bin/java -Djava.security.egd=file:/dev/./urandom -jar /home/deployments/apps/client-0.0.1-SNAPSHOT.war
